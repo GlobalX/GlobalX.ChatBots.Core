4e42dbe8b8ec5f1f60ca1f1125f94230f56a9b61
Laura Berger-Uichanco <laura.berger@globalx.com.au>
2020-01-30 08:05:01 +1000

    feat: add message type for lists

--- src/GlobalX.ChatBots.Core/Messages/MessagePart.cs
+++ src/GlobalX.ChatBots.Core/Messages/MessagePart.cs
@@ -3,5 +3,6 @@ namespace GlobalX.ChatBots.Core.Messages {
         public MessageType MessageType { get; set; }
         public string Text { get; set; }
         public string UserId { get; set; }
+        public string[] ListItems { get; set; }
     }
 }
